b175b013dcd82aa037634a98c6d1e23dea8479d0
saiprasanth03 <ssaiprasanth333@example.com>
2026-02-15 10:10:41 +0530

    Fix PORT for Render

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  
+  }
 };
 
 module.exports = connectDB;
